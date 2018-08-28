8c28bb9a7d6ca689135be54f721c94d128df609d
Fabio Berger <me@fabioberger.com>
2018-08-28 14:32:52 +0100

    Fix url

--- src/configs.ts
+++ src/configs.ts
@@ -5,7 +5,7 @@ export const TX_DEFAULTS = { gas: 400000 };
 export const MNEMONIC = 'concert load couple harbor equip island argue ramp clarify fence smart topic';
 export const BASE_DERIVATION_PATH = `44'/60'/0'/0`;
 export const GANACHE_CONFIGS: NetworkSpecificConfigs = {
-    rpcUrl: 'http =//127.0.0.1 =8545',
+    rpcUrl: 'http://127.0.0.1:8545',
     networkId: GANACHE_NETWORK_ID,
     forwarderAddress: '0xb69e673309512a9d726f87304c6984054f87a93b',
 };
