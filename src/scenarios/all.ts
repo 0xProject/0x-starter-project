6c924ce79b9cfb841f074ab0ff29dff32da98531
Jacob Evans <jacob@dekz.net>
2018-09-27 21:09:59 +1000

    Add Execute Transaction Cancel Order to all runner

--- src/scenarios/all.ts
+++ src/scenarios/all.ts
@@ -2,6 +2,7 @@ import { providerEngine } from '../provider_engine';
 
 import { scenarioAsync as cancelOrdersUpTo } from './cancel_orders_up_to';
 import { scenarioAsync as executeTransaction } from './execute_transaction';
+import { scenarioAsync as executeTransactionCancelOrder } from './execute_transaction_cancel_order';
 import { scenarioAsync as fillOrderERC20 } from './fill_order_erc20';
 import { scenarioAsync as fillOrderERC721 } from './fill_order_erc721';
 import { scenarioAsync as fillOrderFees } from './fill_order_fees';
@@ -16,6 +17,7 @@ void (async () => {
         await fillOrderERC721();
         await matchOrders();
         await executeTransaction();
+        await executeTransactionCancelOrder();
         await cancelOrdersUpTo();
         await forwarder_buy_erc20_tokens();
         await forwarder_buy_erc721_tokens();
